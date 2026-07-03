977713e5d92716afed1afdf0fb2924c9fb7bf672
mfhsohag01 <md.sohag2001bd@gmail.com>
2026-07-03 08:00:47 +0600

    fix(weather-condition): round temp min, fix broken temp-min icon, add humidity label

--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -1,15 +1,44 @@
 import { useContext } from "react";
+import HazeIcon from "../../assets/haze.svg";
+import CloudIcon from "../../assets/icons/cloud.svg";
+import SnowIcon from "../../assets/icons/snow.svg";
+import SunnyIcon from "../../assets/icons/sunny.svg";
 import PinIcon from "../../assets/pin.svg";
+import RainIcon from "../../assets/rainy.svg";
+import ThunderIcon from "../../assets/thunder.svg";
+
 import WeatherContext from "../../context";
 import { getFormattedDate } from "../../utils/date-utils";
 
 export default function WeatherHeadline() {
   const { weatherData } = useContext(WeatherContext);
   const { temperature, location, climate, time } = weatherData;
+  const getWeatherIcon = (climate) => {
+    switch (climate) {
+      case "Clear":
+        return SunnyIcon;
+      case "Clouds":
+        return CloudIcon;
+      case "Rain":
+        return RainIcon;
+      case "Snow":
+        return SnowIcon;
+      case "Thunderstorm":
+        return ThunderIcon;
+      case "Haze":
+        return HazeIcon;
+      case "Fog":
+        return HazeIcon;
+      case "Mist":
+        return HazeIcon;
+      default:
+        return CloudIcon;
+    }
+  };
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src="./assets/cloud.svg" alt="cloud" />
+        <img src={getWeatherIcon(climate)} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
             {Math.round(temperature)}°C
